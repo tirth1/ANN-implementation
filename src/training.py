d668f888f821feab3b3f03efccc10e07b7fd99e4
tirth1 <emailtirth@gmail.com>
2021-10-10 10:50:55 +0530

    added save model function

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
+import os.path
 from utils.common import read_config
 from utils.data_mgmt import get_data
-from utils.model import create_model
+from utils.model import create_model, save_model
 import argparse
 
 def training(config_path):
@@ -16,6 +17,13 @@ def training(config_path):
     VALIDATION = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_name = config["artifacts"]["model_name"]
+    model_dir = config["artifacts"]["model_dir"]
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    save_model(model, model_name, model_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import time
+import os
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES=10):
     LAYERS = [
@@ -10,5 +12,17 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES=10):
     model_clf = tf.keras.models.Sequential(LAYERS)
     model_clf.summary()
 
-    model_clf.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER, metrics=METRICS)
+    model_clf.compile(loss=LOSS_FUNCTION,
+                      optimizer=OPTIMIZER,
+                      metrics=METRICS)
     return model_clf
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    print(path_to_model)
+    model.save(path_to_model)
